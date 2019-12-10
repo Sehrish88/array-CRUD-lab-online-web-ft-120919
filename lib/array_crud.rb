050ffb79609f668270d50e9cdb40077e750b1d5b
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-10 22:17:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  new_array
+  new_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
